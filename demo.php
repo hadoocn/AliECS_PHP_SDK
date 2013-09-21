c22af66f1fc7c45aa71862d2f51e5c835c492699
hexdata <github@hexdata.cn>
2013-09-22 07:55:10 +0800

    更新

--- demo.php
+++ demo.php
@@ -1,9 +1,16 @@
 <?php
-require_once "ecs.sdk.class.php";
+//ini_set('display_errors',true);
+//error_reporting(E_ALL);
 
-$accessKeyId = '';
-$accessKeySecret = '';
-$ecs=new ECS($accessKeyId,$accessKeySecret);
-print_r($ecs->describeInstanceAttribute('AY120723082752d383204'));
+require_once "ecs.sdk.class.20120913.php";
+
+$ecs=new ECS( array(
+	'accessKeyID' => '你的Key',
+	'accessKeySec' => '你的密钥',
+	'accessGetway' => 'http://ecs.aliyuncs.com'
+) );
+print_r($ecs->describeInstanceAttribute( array(
+	'InstanceId' => 'AY120723082752d3832041'    // 你的云机机编号（注意与主机名可能不一致）
+) ));
 print_r($ecs->describeInstanceTypes());
 ?>
